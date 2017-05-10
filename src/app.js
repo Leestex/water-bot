488b13a4bf4858b05a48a57dfdb24caf24cb640a
Nazar Kuzmenko <nazar.kuzmenko@gmail.com>
2017-05-10 03:24:46 +0300

    feat(router): creates basic router

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 
+import router from './router'
+import log from './modules/logger'
+
 const app = express()
 
 app.use(helmet())
-app.use(morgan('tiny'))
+app.use(morgan('tiny', { stream: log.stream }))
 app.use(bodyParser.json())
+app.use(router)
 
 export default app
