6e0b943ed521d8241730f83a532b5258718605fa
Nazar Kuzmenko <nazar.kuzmenko@gmail.com>
2017-05-11 07:39:43 +0300

    feat(menu): creates call to actions menu

--- src/modules/bot/index.js
+++ src/modules/bot/index.js
@@ -3,12 +3,15 @@ import config from 'config'
 
 import { start, quickReply, defaultMessage } from './conversations'
 import { getRequestFromPayload } from './request'
+import { threadState, callToActions } from './menu'
 
 const bot = new Bot({
   verify: config.get('facebook.verifyToken'),
   token: config.get('facebook.accessToken'),
   app_secret: config.get('facebook.appSecret'),
 })
+
+bot.setThreadSettings(threadState, callToActions)
 
 bot.on('message', async (payload, reply) => {
   const request = await getRequestFromPayload(payload, reply, bot)
---
+++ src/modules/bot/menu/index.js
@@ -0,0 +1,13 @@
+export const threadState = 'existing_thread'
+export const callToActions = [
+  {
+    type: 'postback',
+    title: 'Start',
+    payload: 'START',
+  },
+  {
+    type: 'postback',
+    title: 'Reminder Frequency',
+    payload: 'REMINDER_FREQUENCY',
+  },
+]
