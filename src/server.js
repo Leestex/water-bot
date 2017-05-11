40b835731613d1509c83797284f78591d72f2773
Nazar Kuzmenko <nazar.kuzmenko@gmail.com>
2017-05-11 06:43:16 +0300

    feat(db): adds mongodb connection via mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import config from 'config'
+import mongoose from 'mongoose'
+import Promise from 'bluebird'
 
 import app from './app'
 import log from './modules/logger'
+
+mongoose.Promise = Promise
+
+function onConnected () {
+  mongoose.connections.forEach(connection => {
+    log.info(`[ MONGODB ] HOST: ${connection.host}`)
+    log.info(`[ MONGODB ] PORT: ${connection.port}`)
+    log.info(`[ MONGODB ] DB: ${connection.name}`)
+  })
+}
 
 function onListen () {
   const host = this.address().address
@@ -10,5 +22,7 @@ function onListen () {
   log.info(`[ SERVER ] HOST: ${host}`)
   log.info(`[ SERVER ] PORT: ${port}`)
 }
+
+mongoose.connect(config.get('database.mongodb.uri'), onConnected)
 
 app.listen(config.get('server.port'), onListen)
